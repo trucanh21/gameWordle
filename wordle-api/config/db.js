5a16aa4f340715bdd738770f7f7b72970e5e17bd
trucanh21 <taxuanlan2002@gmail.com>
2025-03-13 14:13:21 +0700

    update file database

--- wordle-api/config/db.js
+++ wordle-api/config/db.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql2');
+require('dotenv').config(); // Load biến môi trường từ .env
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'Jiyeon12345!', // Thay đổi mật khẩu MySQL của bạn
-    database: 'wordle_game',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
 });
 
-module.exports = pool.promise(); 
+module.exports = pool.promise();
